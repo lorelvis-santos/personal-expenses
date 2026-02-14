7fe4c30b9bdc98ee5f873fb98411fe94d9257f07
Lorelvis Santos <santoslorelvis@gmail.com>
2026-02-14 02:55:08 -0400

    refactor: move all contents from PersonalExpenses/ to root

---
+++ PersonalExpenses.Data/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+namespace PersonalExpenses.Data.Interfaces;
+
+public interface IRepository<T>
+{
+    bool Add(T entity);
+    List<T> GetAll();
+    T? GetById(string id);
+    bool Update(T entity);
+    bool Delete(string id);
+}
---
+++ PersonalExpenses.Data/Repositories/BaseRepository.cs
@@ -0,0 +1,64 @@
+using PersonalExpenses.Data.Interfaces;
+using PersonalExpenses.Data.Contexts;
+using PersonalExpenses.Entities;
+
+namespace PersonalExpenses.Data.Repositories;
+
+public abstract class BaseRepository<T> : IRepository<T> where T : BaseModel
+{
+    protected readonly IFileContext<T> _context;
+    protected readonly string _fileName;
+
+    protected BaseRepository(IFileContext<T> context, string fileName)
+    {
+        _context = context;
+        _fileName = fileName;
+    }
+
+    public List<T> GetAll()
+    {
+        return _context.LoadData(_fileName);
+    }
+
+    public T? GetById(string id)
+    {
+        List<T> items = GetAll();
+        return items.FirstOrDefault(item => item.Id == id);
+    }
+
+    public bool Add(T item)
+    {
+        List<T> items = GetAll();
+        items.Add(item);
+        return _context.SaveData(_fileName, items);
+    }
+
+    public bool Update(T item)
+    {
+        List<T> items = GetAll();
+        int index = items.FindIndex(i => i.Id == item.Id);
+
+        if (index == -1)
+        {
+            return false;
+        }
+
+        items[index] = item;
+        return _context.SaveData(_fileName, items);
+    }
+
+    public bool Delete(string id)
+    {
+        List<T> items = GetAll();
+        T? itemToRemove = GetById(id);
+
+        if (itemToRemove == null)
+        {
+            return false;
+        }
+
+        items.Remove(itemToRemove);
+        return _context.SaveData(_fileName, items);
+    }
+}
+
