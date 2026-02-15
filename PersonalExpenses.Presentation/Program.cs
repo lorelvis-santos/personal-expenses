f97858e4c4339b587f6e9131448d367d88c4af2e
Lorelvis Santos <santoslorelvis@gmail.com>
2026-02-15 00:50:24 -0400

    feat: initialize HomeMenu and HomeController for application entry point

--- PersonalExpenses.Presentation/Program.cs
+++ PersonalExpenses.Presentation/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿// using PersonalExpenses.Data.Contexts;
+using PersonalExpenses.Presentation.Controllers;
+using PersonalExpenses.Presentation.Views;
+
+// IFileContext context = new JsonFileContext();
+
+HomeMenu homeMenu = new();
+HomeController homeController = new(homeMenu);
+
+bool homeLoop = true;
+
+while (homeLoop)
+{
+    homeLoop = homeController.Execute();
+}
