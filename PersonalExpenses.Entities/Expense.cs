1ede8090bcdabb1cbb4c8a0e7a9e862dc012039a
Lorelvis Santos <santoslorelvis@gmail.com>
2026-02-15 21:04:59 -0400

    feat: enforce required Description property and initialize Date in Expense model

--- PersonalExpenses.Entities/Expense.cs
+++ PersonalExpenses.Entities/Expense.cs
@@ -3,7 +3,7 @@ namespace PersonalExpenses.Entities;
 public class Expense : BaseModel
 {
     public decimal Amount { get; set; }
-    public string? Description { get; set; }
-    public DateTime Date { get; set; }
+    public required string Description { get; set; }
+    public DateTime Date { get; set; } = DateTime.Now;
     public required string CategoryId { get; set; }
 }
